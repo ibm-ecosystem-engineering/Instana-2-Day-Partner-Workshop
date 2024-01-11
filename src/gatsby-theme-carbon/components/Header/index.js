93d38344cb51c3430091b4cad0046c3764e74582
Jyoti Rani <jyotirani10@in.ibm.com>
2024-01-11 12:18:24 +0530

    updated to 2-days workshop

--- src/gatsby-theme-carbon/components/Header/index.js
+++ src/gatsby-theme-carbon/components/Header/index.js
@@ -3,7 +3,7 @@ import Header from 'gatsby-theme-carbon/src/components/Header';
 
 const CustomHeader = (props) => (
   <Header {...props}>
-    &nbsp;<span>Instana Partner 1-Day Technical Workshop</span>
+    &nbsp;<span>Instana Partner 2-Days Technical Workshop</span>
   </Header>
 );
 
